023f7d97f12153a6befcba0564e61f6fab5637a6
WhiteVax <WhiteVax@gmail.com>
2021-11-19 13:12:29 +0200

    1.5. Поля объекта [#166083]

--- src/main/java/ru/job4j/oop/Cat.java
+++ src/main/java/ru/job4j/oop/Cat.java
@@ -1,8 +1,28 @@
 package ru.job4j.oop;
 
 public class Cat {
+
+    private  String food;
+
+    private String name;
+
+    public void eat(String meat) {
+        this.food = meat;
+    }
+
+    public void getNick(String nick) {
+        this.name = nick;
+    }
+
+    public void show() {
+        System.out.println("The cat name is " + this.name);
+        System.out.println("The cat ate " + this.food);
+    }
+
     public static void main(String[] args) {
-        Cat peppy = new Cat();
         Cat sparky = new Cat();
+        sparky.eat("fish");
+        sparky.getNick("sparky");
+        sparky.show();
     }
 }
