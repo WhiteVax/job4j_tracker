5520c71b9b77042d8caa053b58f1ea9af817d4d9
WhiteVax <WhiteVax@gmail.com>
2021-12-01 21:26:53 +0200

    2. Перегрузка конструктора [#173890]

--- src/main/java/ru/job4j/tracker/Item.java
+++ src/main/java/ru/job4j/tracker/Item.java
@@ -1,8 +1,26 @@
 package ru.job4j.tracker;
 
 public class Item {
+
     private int id;
+
     private String name;
+
+    public Item() {
+    }
+
+    public Item(int id) {
+        this.id = id;
+    }
+
+    public Item(String name) {
+        this.name = name;
+    }
+
+    public Item(String name, int id) {
+        this.name = name;
+        this.id = id;
+    }
 
     public int getId() {
         return id;
