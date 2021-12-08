55017c4257b8bc9dd803fdd009cd7361f8b6f6e8
WhiteVax <WhiteVax@gmail.com>
2021-12-08 21:33:49 +0200

    3. Массивы и модели. [#174892 #237646]

--- src/main/java/ru/job4j/pojo/Library.java
+++ src/main/java/ru/job4j/pojo/Library.java
@@ -6,30 +6,23 @@ public class Library {
         Book secondBook = new Book("City of the Dead", 320);
         Book thirdBook = new Book("School's Out - Forever", 448);
         Book fourthBook = new Book("The Shadow", 400);
-
         Book[] books = new Book[4];
         books[0] = firstBook;
         books[1] = secondBook;
         books[2] = thirdBook;
         books[3] = fourthBook;
-
         for (Book item : books) {
             System.out.println(item.getName() + " : " + item.getNumberOfPages());
         }
-
         System.out.println("");
-
         Book temp;
         temp = books[3];
         books[3] = books[0];
         books[0] = temp;
-
         for (Book item : books) {
             System.out.println(item.getName() + " : " + item.getNumberOfPages());
         }
-
         System.out.println("");
-
         for (int index = 0; index < books.length; index++) {
             if ("Clean code".equals(books[index].getName())) {
                 System.out.println(books[index].getName() + " : " + books[index].getNumberOfPages());
