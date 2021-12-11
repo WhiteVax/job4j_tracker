31e87e34da9a0732e9571f81d00d428131a0df4b
WhiteVax <WhiteVax@gmail.com>
2021-12-11 21:12:14 +0200

    5. Tracker - хранилище [#396]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,23 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        return Arrays.copyOf(items, size);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] rsl = new Item[items.length];
+        int sizeArray = 0;
+
+        for (int index = 0; index < size; index++) {
+            if (key.equals(items[index].getName())) {
+                rsl[sizeArray] = items[index];
+                sizeArray++;
+            }
+        }
+        return Arrays.copyOf(rsl, sizeArray);
     }
 
     public Item findById(int id) {
