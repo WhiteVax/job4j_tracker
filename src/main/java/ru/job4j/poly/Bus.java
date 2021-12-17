de7a06efc64cf0ea620d7ccd4ddb76b98062eb40
WhiteVax <WhiteVax@gmail.com>
2021-12-17 20:34:15 +0200

    3. Интерфейс Input [#181092 #240911]

--- src/main/java/ru/job4j/poly/Bus.java
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -4,14 +4,17 @@ public class Bus implements Transport {
 
     @Override
     public void run() {
+        boolean run = true;
     }
 
     @Override
     public void passengers(int passengers) {
+        int count = passengers;
     }
 
     @Override
     public double refuel(double refuel) {
-        return refuel * 43.40;
+        double price = 43.50;
+        return refuel * price;
     }
 }
