2e71594578017b6f9e8200675aff96800f4acb42
WhiteVax <WhiteVax@gmail.com>
2022-01-25 18:03:01 +0200

    6. Изменить программу Tracker из 2-го модуля [#10039]

--- src/main/java/ru/job4j/tracker/FindActionByName.java
+++ src/main/java/ru/job4j/tracker/FindActionByName.java
@@ -14,21 +14,6 @@ public class FindActionByName implements UserAction {
         return "Find item by name.";
     }
 
-//    @Override
-//    public boolean execute(Input input, Tracker tracker) {
-//        out.println("=== Find items by name ===");
-//        String name = input.askStr("Enter name: ");
-//        Item[] items = tracker.findByName(name);
-//        if (items.length > 0) {
-//            for (Item item : items) {
-//                out.println(item);
-//            }
-//        } else {
-//            out.println("Заявки с именем: " + name + " не найдены.");
-//        }
-//        return true;
-//    }
-
     @Override
     public boolean execute(Input input, Tracker tracker) {
         out.println("=== Find items by name ===");
