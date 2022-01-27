4c3247d71b7f17dd33a2fe800ce2789317b4f4db
WhiteVax <WhiteVax@gmail.com>
2022-01-27 13:18:26 +0200

    0. Map, HashMap [#212652]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,24 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class UsageMap {
+    public static void main(String[] args) {
+
+        Map<String, String> storage = new HashMap<>();
+        storage.put("test@gmail.com", "name surname");
+        storage.put("second@gmail.com", "name surname");
+
+        for (Map.Entry<String, String> entry : storage.entrySet()) {
+            String key = entry.getKey();
+            String value = entry.getValue();
+            System.out.println(key + " : " + value);
+        }
+
+        for (String key : storage.keySet()) {
+            String value = storage.get(key);
+            System.out.println(key + " : " + value);
+        }
+    }
+}
