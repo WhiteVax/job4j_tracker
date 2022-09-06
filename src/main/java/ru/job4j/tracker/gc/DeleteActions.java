e640fe9f15a7f7260d7369fd2f50c0eb163e1b34
WhiteVax <WhiteVax@gmail.com>
2022-09-06 21:51:17 +0300

    1. Профилирование. [#1591 #339981]

---
+++ src/main/java/ru/job4j/tracker/gc/DeleteActions.java
@@ -0,0 +1,33 @@
+package ru.job4j.tracker.gc;
+
+import ru.job4j.tracker.*;
+
+import java.util.List;
+
+public class DeleteActions implements UserAction {
+    private final Output out;
+
+    public DeleteActions(Output out) {
+        this.out = out;
+    }
+
+    @Override
+    public String name() {
+        return "Delete item.";
+    }
+
+    @Override
+    public boolean execute(Input input, Store store) {
+        out.println("=== Delete items ===");
+        int id = input.askInt("Enter count: ");
+        List<Item> ids = store.findAll();
+        for (int i = 0; i < id; i++) {
+            if (store.delete(ids.get(i).getId())) {
+                out.println("Заявка удалена успешно.");
+            } else {
+                out.println("Ошибка удаления заявки.");
+            }
+        }
+        return true;
+    }
+}
