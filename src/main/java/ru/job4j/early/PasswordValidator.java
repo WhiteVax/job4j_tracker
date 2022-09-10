02e9d4fb1aece711b16256c9dd496c197cf714eb
WhiteVax <WhiteVax@gmail.com>
2022-09-10 16:08:11 +0300

    4. Принципы раннего возврата и охранных выражений [#504875 #338993]

--- src/main/java/ru/job4j/early/PasswordValidator.java
+++ src/main/java/ru/job4j/early/PasswordValidator.java
@@ -1,7 +1,5 @@
 package ru.job4j.early;
 
-import java.util.regex.Pattern;
-
 public class PasswordValidator {
 
     public static String validate(String password) {
@@ -9,20 +7,31 @@ public class PasswordValidator {
             throw new IllegalArgumentException(String.format("Wrong length password %d.", password.length()));
         }
 
-        if (!password.matches("(.*[a-z].*)(.*[A-Z].*)")) {
+        if (password.equals(password.toLowerCase()) || password.equals(password.toUpperCase())) {
             throw new IllegalArgumentException(String.format(
                     "Does not contain a top or bottom character %s.", password));
         }
 
-        if (!password.matches("(.*\\W.*)(.*\\d.*)")) {
+        boolean isDigit = false;
+        boolean isSpace = false;
+        for (var ch: password.toCharArray()) {
+            if (Character.isDigit(ch)) {
+                isDigit = true;
+            }
+            if (!Character.isLetterOrDigit(ch)) {
+                isSpace = true;
+            }
+        }
+
+        if (!isDigit || !isSpace) {
             throw new IllegalArgumentException(String.format(
                     "Does not contain a number or symbol %s.", password));
         }
 
-        if (Pattern.compile(Pattern.quote("qwerty"), Pattern.CASE_INSENSITIVE).matcher(password).find()
-                || Pattern.compile(Pattern.quote("password"), Pattern.CASE_INSENSITIVE).matcher(password).find()
-                || Pattern.compile(Pattern.quote("user"), Pattern.CASE_INSENSITIVE).matcher(password).find()
-                || Pattern.compile(Pattern.quote("admin"), Pattern.CASE_INSENSITIVE).matcher(password).find()
+        if (password.toLowerCase().contains("qwerty")
+                || password.toLowerCase().contains("password")
+                || password.toLowerCase().contains("user")
+                || password.toLowerCase().contains("admin")
                 || password.contains("12345")) {
             throw new IllegalArgumentException(String.format("Password is simple %s.", password));
         }
